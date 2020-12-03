c5a0ae69cd2b403eb301fde88ef4fd798727d626
SScott08 <68204195+SScott08@users.noreply.github.com>
2020-12-02 23:52:26 -0800

    multiple points

--- Mapping_Single_Points/js/logic.js
+++ Mapping_Single_Points/js/logic.js
@@ -2,23 +2,15 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
-// Get data from cities.js
-let cityData = cities;
-
-// Loop through the cities array and create one marker for each city.
-cities.forEach(function(city) {
-  console.log(city)
- });
-
- // Loop through the cities array and create one marker for each city.
-cityData.forEach(function(city) {
-  console.log(city)
-  L.marker(city.location)
-  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population + "</h3>")
-.addTo(map);
-});
+//  Add a marker to the map for Los Angeles, California.
+let marker = L.marker([34.0522, -118.2437]).addTo(map);
+L.circle([34.0522, -118.2437], {
+  radius: 300,
+  color: "black",
+  fillColor: '#ffffa1'
+}).addTo(map);
 
    
 // We create the tile layer that will be the background of our map.
@@ -27,7 +19,6 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
     maxZoom: 18,
     accessToken: API_KEY
 });
-
 
 // Then we add our 'streets' tile layer to the map.
 streets.addTo(map);
--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -1,6 +1,11 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
+// Loop through the cities array and create one marker for each city.
+cities.forEach(function(city) {
+    console.log(city)
+   });
+
 // Create the map object with a center and zoom level.
 let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
